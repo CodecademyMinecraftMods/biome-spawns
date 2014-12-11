96430a7b532d8f51c2555cd4361736ae986cfa26
Latent Leprechaun <LatentLeprechaun@users.noreply.github.com>
2014-12-10 16:42:47 -0800

    Added all the basic stuff for a 1.7.10 Forge mod.

--- forge-1.6.4-9.11.1.965-src/forge/mcp/src/minecraft/biomespawns/common/BiomeSpawnMod.java
+++ forge-1.6.4-9.11.1.965-src/forge/mcp/src/minecraft/biomespawns/common/BiomeSpawnMod.java
@@ -1,5 +1,17 @@
 package biomespawns.common;
 
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = BiomeSpawnMod.MODID, version = BiomeSpawnMod.VERSION)
 public class BiomeSpawnMod {
+  
+  public static final String MODID = "biomeSpawnMod";
+  public static final String VERSION = "0.0.1";
+  
+  public void init(FMLInitializationEvent event){
+    
+  }
 
 }
